4931fa21f443c8c962a42f788d9f1df9781a798f
IlyaStudent <ilyaprepodavatel@gmail.com>
2025-11-28 19:28:21 +0300

    IamResponse add

--- src/main/java/com/social_media/iam_service/service/PostService.java
+++ src/main/java/com/social_media/iam_service/service/PostService.java
@@ -1,7 +1,6 @@
 package com.social_media.iam_service.service;
 
 import com.social_media.iam_service.model.dto.post.PostDTO;
-import com.social_media.iam_service.model.entity.Post;
 import com.social_media.iam_service.model.response.IamResponse;
 import jakarta.validation.constraints.NotNull;
 
